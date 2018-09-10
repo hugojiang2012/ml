65104c4ce90b255b566a96c7cc58a804f2dfcc1a
jiangjinping19861013 <sea_jiangjinping@163.com>
2018-09-10 22:35:27 +0800

    first commit

---
+++ ml/code/tfidf/tfidtest2.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+_author_ ="hugojiang2012"
+import  jieba
+import jieba.posseg as pseg
+import os
+import sys
+from sklearn import feature_extraction
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+
+if __name__ == "__main__":
+    co
